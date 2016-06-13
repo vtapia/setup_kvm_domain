7ae71bfbdc0e9b0b4040908a74dd77b4d1d9ea45
Ubuntu <ubuntu@porygon.svvp>
2016-06-13 09:38:07 +0000

    Added file configuration support (rushed)

--- setup_kvm_domain.py
+++ setup_kvm_domain.py
@@ -1,4 +1,5 @@
 import libvirt
+import yaml
 import time
 import sys
 import os
@@ -38,18 +39,31 @@ def pinning_arg(arg):
 def read_args():
     parser = argparse.ArgumentParser(
             description='Change the "fixed" resources assigned to a VM (cpu, mem, vqueues, cpu pinning) in an non-interactive manner, using transient domains')
-    parser.add_argument('-r', '--restart', help='Restart VM to apply changes.', action="store_true")
-    parser.add_argument('-v', '--verbose', help='Show debug messages', action="store_true")
-    parser.add_argument('-d', '--dump', help='Dump the new domain XML but do not apply it.', action="store_true")
-    parser.add_argument('-m', '--memory', type=int,  help='Memory in MB.')
-    parser.add_argument('-c', '--cpu', type=int,  help='vCPU number.')
-    parser.add_argument('-q', '--queues', type=int,  help='virtqueues number.')
-    parser.add_argument('-p', '--pin', type=pinning_arg, help='CPU pinning in [0-9]-[0-9] format (e.g. "1-8").\
+    subparsers = parser.add_subparsers(help='Input type: cmd or (config) file.')
+
+    # Read from file
+    file_parser = subparsers.add_parser('file', help='Config file')
+    file_parser.add_argument('-f', '--file', type=str, help='Config file to read from', required=True)
+    file_parser.add_argument('-o', '--option', type=int, choices=range(1,12), help='Option defined in the config file (e.g. Q8)', required=True)
+    file_parser.add_argument('-r', '--restart', help='Restart VM to apply changes.', action="store_true")
+    file_parser.add_argument('-v', '--verbose', help='Show debug messages', action="store_true")
+    file_parser.add_argument('-d', '--dump', help='Dump the new domain XML but do not apply it.', action="store_true")
+    file_parser.add_argument('vm', type=str, help='VM name')
+
+    # Read from cmdline
+    cmd_parser = subparsers.add_parser('cmd', help='Oneline config')
+    cmd_parser.add_argument('-r', '--restart', help='Restart VM to apply changes.', action="store_true")
+    cmd_parser.add_argument('-v', '--verbose', help='Show debug messages', action="store_true")
+    cmd_parser.add_argument('-d', '--dump', help='Dump the new domain XML but do not apply it.', action="store_true")
+    cmd_parser.add_argument('-m', '--memory', type=int,  help='Memory in MB.')
+    cmd_parser.add_argument('-c', '--cpu', type=int,  help='vCPU number.')
+    cmd_parser.add_argument('-q', '--queues', type=int,  help='virtqueues number.')
+    cmd_parser.add_argument('-p', '--pin', type=pinning_arg, help='CPU pinning in [0-9]-[0-9] format (e.g. "1-8").\
                         The second value must be higher than the first one, but lower than the amount of cores in the physical host (check /proc/cpuinfo) ')
-    parser.add_argument('vm', type=str, help='VM name')
+    cmd_parser.add_argument('vm', type=str, help='VM name')
+
     args = parser.parse_args()
 
-    logger.debug("- Using config file: %s" % args)
     return args
 
 
@@ -76,7 +90,7 @@ def vm_status(dom):
     if not os.path.exists(BACKUP_DIR):
         os.makedirs(BACKUP_DIR)
 
-    backup_file = BACKUP_DIR + "/" + dom.name() + '_' + str(time.time() + '.xml')
+    backup_file = BACKUP_DIR + "/" + dom.name() + '_' + str(time.time()) + '.xml'
 
     with open(backup_file, 'w') as f:
         f.write(raw_xml)
@@ -93,7 +107,7 @@ def vm_edit_xml(dom, args):
         logger.debug("- Setting vCPU to %s" % str(args.cpu))
         tree.find('.//vcpu').text = str(args.cpu)
     if args.memory is not None:
-        args.memory = args.memory * 1024
+        args.memory = int(args.memory) * 1024
         logger.debug("- Setting memory and currentMemory to %s" % str(args.memory))
         tree.find('.//memory').text = str(args.memory)
         tree.find('.//currentMemory').text = str(args.memory)
@@ -118,6 +132,16 @@ def main():
 
     if args.verbose is True:
         logger.setLevel(logging.DEBUG)
+
+    if args.file is not None:
+        logger.debug("- Using config file: %s" % args.file)
+        with open(args.file, 'r') as f:
+            cfg = yaml.load(f)
+            option = str(args.option)
+            args.cpu = cfg[option]['cpu']
+            args.memory = cfg[option]['memory']
+            args.queues = cfg[option]['queues']
+            args.pin = cfg[option]['pin']
 
     lv = libvirt.open('qemu:///system')
     if lv is None:
